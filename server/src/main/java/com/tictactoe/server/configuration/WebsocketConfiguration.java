2e1d43419cba2f842b5653e646157ef627a71838
Noam Levy <noam5003@gmail.com>
2023-01-23 21:15:18 +0200

    added rest controller and websocket configuration

---
+++ server/src/main/java/com/tictactoe/server/configuration/WebsocketConfiguration.java
@@ -0,0 +1,22 @@
+package com.tictactoe.server.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // allowedOrigins set to all but may be changed for security reasons in the future.
+        registry.addEndpoint("/doTurn", "/connect").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
+    }
+}
---
+++ server/src/main/java/com/tictactoe/server/controlles/TicTacToeController.java
@@ -0,0 +1,44 @@
+package com.tictactoe.server.controlles;
+
+import com.tictactoe.server.model.Game;
+import com.tictactoe.server.model.GameTurn;
+import com.tictactoe.server.model.Player;
+import com.tictactoe.server.services.GameService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tictactoe")
+@AllArgsConstructor
+public class TicTacToeController {
+    private SimpMessagingTemplate messagingTemplate;
+    private GameService service;
+
+    @PostMapping("/init")
+    public ResponseEntity<Game> initializeGame(@RequestBody Player player) {
+        return ResponseEntity.ok(this.service.initializeGame(player));
+    }
+
+    @PostMapping("/connect")
+    public ResponseEntity<Game> connectToGame(
+            @RequestBody Player player,
+            @RequestParam(name="gameId", defaultValue="") String gameId) {
+        Game game;
+        if (gameId.equals(""))
+            game =  this.service.connectToRandomGame(player);
+        else
+            game = this.service.connectToSpecificGame(gameId, player);
+
+        this.messagingTemplate.convertAndSend("/topic/connection/" + game.getGameId(), game);
+        return ResponseEntity.ok(game);
+    }
+
+    @PostMapping("/doTurn")
+    public ResponseEntity<Game> doTurn(@RequestBody GameTurn turn) {
+        Game game = this.service.doTurn(turn);
+        this.messagingTemplate.convertAndSend("/topic/progress/" + game.getGameId(), game);
+        return ResponseEntity.ok(game);
+    }
+}
